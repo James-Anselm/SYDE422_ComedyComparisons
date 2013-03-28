22fb4c17ca18fd9c8f0c7faec161e4a8307ea43b
DouglasSherk <github@sherk.me>
2013-03-28 16:19:41 -0400

    add dat parser

---
+++ csvparse.m
@@ -0,0 +1,2 @@
+% Read properties of file.
+[id, commentCount, dislikeCount, favoriteCount, likeCount, viewCount, funnyWordsInTitle, funnyWordsInDesc] = textread('download.csv', '%s %d %d %d %d %d %d %d');
